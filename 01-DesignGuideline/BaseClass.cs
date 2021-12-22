7a438273af8234dda3d64cf889d9c5ef697cf314
Миша Е <eliseevmp@live.com>
2021-12-22 19:44:44 +0300

    Fix BaseClass.

--- 01-DesignGuideline/BaseClass.cs
+++ 01-DesignGuideline/BaseClass.cs
@@ -8,97 +8,90 @@
  * *******************************************************************************/
 
 using System;
-using System.Text;
-using System.IO;
 using System.ComponentModel;
-namespace codest
+using System.IO;
+using System.Text;
+
+namespace Codest
 {
-    #region public delegate void NullParamEvent();
     /// <summary>
-    /// 无参数且无返回值的托管类型
+    /// 无参数且无返回值的托管类型.
     /// </summary>
     public delegate void NullParamEvent();
-    #endregion
 
     /// <summary>
     /// Icyplayer所有类的基类
-    /// 实现IDisposable, ICloneable接口
+    /// 实现IDisposable, ICloneable接口.
     /// </summary>
     public abstract class BaseClass : IDisposable, ICloneable
     {
-        #region 成员变量
         /// <summary>
-        /// 托管资源容器
+        /// 托管资源容器.
         /// </summary>
-        private Container components = null; 
-        /// <summary>
-        /// 指示对象是否已经析构
-        /// </summary>
-        protected bool disposed = false;
-        #endregion
+        private Container components = null;
 
-        #region 构造/析构函数
+        /// <summary>
+        /// 指示对象是否已经析构.
+        /// </summary>
+        protected internal bool disposed = false;
+
         /// <summary>
         /// BaseClass构造函数
         /// </summary>
-        public BaseClass()
-        { 
+        protected BaseClass()
+        {
         }
+
         /// <summary>
         /// BaseClass析构函数
         /// </summary>
         ~BaseClass()
         {
-            Dispose(false);
+            this.Dispose(false);
         }
-        #endregion
 
-        #region protected virtual void Dispose(bool disposing)
         /// <summary>
-        /// 撤销对象
+        /// 完成对象的浅复制.
         /// </summary>
-        /// <param name="disposing">false：系统调用，true：手动调用</param>
-        protected virtual void Dispose(bool disposing)
-        {
-            if (disposed) return;
-            if (disposing)
-            {
-                //释放托管资源
-                if (components != null)
-                {
-                    components.Dispose();
-                }
-                disposed = true;
-            }
-            //释放非托管资源
-            //base.Dispose(disposing);
-        }
-        #endregion
-
-        #region ICloneable 成员
-        /// <summary>
-        /// 完成对象的浅复制
-        /// </summary>
-        /// <returns>对象的副本</returns>
+        /// <returns>对象的副本.</returns>
         public virtual object Clone()
         {
-            return MemberwiseClone();
+            return this.MemberwiseClone();
         }
 
-        #endregion
-
-        #region IDisposable 成员
         /// <summary>
-        /// 显式撤销对象
+        /// 显式撤销对象.
         /// </summary>
-        public virtual void Dispose()
+        public void Dispose()
         {
-            Dispose(true);
+            this.Dispose(true);
             GC.SuppressFinalize(this);
         }
 
-        #endregion
+        /// <summary>
+        /// 撤销对象.
+        /// </summary>
+        /// <param name="disposing">false：系统调用，true：手动调用.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (this.disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                // 释放托管资源
+                if (this.components != null)
+                {
+                    this.components.Dispose();
+                }
+
+                this.disposed = true;
+            }
+
+            // 释放非托管资源
+            // base.Dispose(disposing);
+        }
     }
-
-
 }
