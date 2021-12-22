cd62125c0feb7d3d012591b5ff210a2be8cb42ad
Миша Е <eliseevmp@live.com>
2021-12-22 14:06:45 +0300

    Fix DataUpdater styles.

--- 01-DesignGuideline/Data/DataUpdater.cs
+++ 01-DesignGuideline/Data/DataUpdater.cs
@@ -7,119 +7,111 @@
  * * 内容摘要：
  * *******************************************************************************/
 
-
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Data;
+using System.Text;
 
-namespace codest.Data
+namespace Codest.Data
 {
     /// <summary>
-    /// 所有数据库更新器的基类
+    /// 所有数据库更新器的基类.
     /// </summary>
     public abstract class DataUpdater : BaseClass
     {
-        #region 成员变量
+        private int updaterID;
         private bool autoRelease;
-        internal int updaterID;
-        #endregion
 
-        #region 接口封装
-        /// <summary>
-        /// 指示调用Update()后是否自动释放当前更新器
-        /// 默认为：true
-        /// </summary>
-        public bool AutoRelease
-        {
-            get { return autoRelease; }
-            set { autoRelease = value; }
-        }
-        #endregion
-
-        #region 构造/析构函数
         /// <summary>
         /// 构造函数
         /// </summary>
-        /// <param name="id">分配容器中唯一的ID</param>
-        public DataUpdater(int id)
+        /// <param name="id">分配容器中唯一的ID.</param>
+        protected DataUpdater(int id)
         {
-            autoRelease = true;
-            updaterID = id;
+            this.autoRelease = true;
+            this.updaterID = id;
         }
-        /// <summary>
-        /// 析构函数
-        /// </summary>
-        ~DataUpdater()
-        {
-        }
-        #endregion
 
-        #region  protected override void Dispose(bool disposing)
         /// <summary>
-        /// 释放由当前对象控制的所有资源
+        /// 指示调用Update()后是否自动释放当前更新器
+        /// 默认为：true.
         /// </summary>
-        /// <param name="disposing">显式调用</param>
-        protected override void Dispose(bool disposing)
+        public bool AutoRelease
         {
-            if (disposed) return;
-            if (disposing)
-            {
-                //释放托管资源
-            }
-            //释放非托管资源
-            base.Dispose(disposing);
+            get { return this.autoRelease; }
+            set { this.autoRelease = value; }
         }
-        #endregion
 
-        #region  protected virtual void DecideRelease()
         /// <summary>
-        /// 决定是否自动释放更新器
+        /// Gets or sets updaterID.
         /// </summary>
-        protected virtual void DecideRelease()
+        /// <value>updaterID.</value>
+        internal int UpdaterID
         {
-            if (autoRelease)
-                Release();
+            get { return this.updaterID; }
+            set { this.updaterID = value; }
         }
-        #endregion
-
-        #region public abstract DataTable SelectWithUpdate(string SQLCmd)
 
         /// <summary>
         /// 使对象将进入修改模式状态
         /// 用户可以修改返回结果，包括删除、修改和增加行
         /// 再调用Update(DataTable)进行更新操作
-        /// 此后，对象将退出修改模式状态
+        /// 此后，对象将退出修改模式状态.
         /// </summary>
-        public abstract DataTable SelectWithUpdate(string SQLCmd);
-        #endregion
-
-        #region public abstract DataTable InsertMode(string TableName)
+        /// <param name="sQLCmd">some param.</param>
+        /// <returns>something.</returns>
+        public abstract DataTable SelectWithUpdate(string sQLCmd);
 
         /// <summary>
         /// 使对象进入修改模式
         /// 用户可以在返回表结构的DataTable中添加数据
         /// 再调用Update(DataTable)进行更新操作
-        /// 此后，对象将退出修改模式状态
+        /// 此后，对象将退出修改模式状态.
         /// </summary>
-        public abstract DataTable InsertMode(string TableName);
-        #endregion
-
-        #region public abstract void Update(System.Data.DataTable DataTableSource)
+        /// <param name="tableName">some param.</param>
+        /// <returns>something.</returns>
+        public abstract DataTable InsertMode(string tableName);
 
         /// <summary>
-        /// 关闭修改模式,并根据DataTable进行更新操作
+        /// 关闭修改模式,并根据DataTable进行更新操作.
         /// </summary>
-        public abstract void Update(System.Data.DataTable DataTableSource);
-        #endregion
+        /// <param name="dataTableSource">some param.</param>
+        public abstract void Update(System.Data.DataTable dataTableSource);
 
-        #region  public abstract void Release()
         /// <summary>
-        /// 释放当前更新器
+        /// 释放当前更新器.
         /// </summary>
         public abstract void Release();
-        #endregion
 
+        /// <summary>
+        /// 释放由当前对象控制的所有资源.
+        /// </summary>
+        /// <param name="disposing">显式调用.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (this.disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                // 释放托管资源
+            }
+
+            // 释放非托管资源
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// 决定是否自动释放更新器.
+        /// </summary>
+        protected virtual void DecideRelease()
+        {
+            if (this.autoRelease)
+            {
+                this.Release();
+            }
+        }
     }
-
 }
