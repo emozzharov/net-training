5cf2495bc38ef45ee2e949b52a2a7a522a195828
Peter Sviryd <Peter.blr@yandex.ru>
2022-01-01 19:07:46 +0300

    try 2

--- 06-IQueryable/IQueryable/PeopleDbQueryProvider.cs
+++ 06-IQueryable/IQueryable/PeopleDbQueryProvider.cs
@@ -10,26 +10,32 @@ namespace IQueryableTask
 {
     public class PeopleDbQueryProvider : IQueryProvider
     {
+        private readonly IQueryProvider _provider;
+
         public IQueryable CreateQuery(Expression expression)
         {
             // TODO: Implement CreateQuery
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return new People(expression);
 
         }
 
         public IQueryable<TResult> CreateQuery<TResult>(Expression expression)
         {
             // TODO: Implement CreateQuery
-            throw new NotImplementedException();
-
+            //throw new NotImplementedException();
+            return (IQueryable<TResult>)new People(expression);
         }
 
         public object Execute(Expression expression)
         {
             // TODO: Implement Execute
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return Person.Execute(expression, false);
 
         }
+
+        //public object Execute(Expression expression) => _provider.Execute(expression);
 
         public TResult Execute<TResult>(Expression expression)
         {
@@ -37,9 +43,9 @@ namespace IQueryableTask
             throw new NotImplementedException();
 
             // HINT: Use GetSqlQuery to build query and pass the query to PersonService
-
-
         }
+
+        //public TResult Execute<TResult>(Expression expression) => _provider.Execute<TResult>(expression);
 
         /// <summary>
         /// Generates YQL Query
@@ -54,6 +60,27 @@ namespace IQueryableTask
             // HINT: This method is not part of IQueryProvider interface and is used here only for tests.
             // HINT: To transform expression to sql query create a class derived from ExpressionVisitor
             // HINT: Read the tutorial https://msdn.microsoft.com/en-us/library/bb546158.aspx for more info
+        }
+
+        internal class InnermostWhereFinder : ExpressionVisitor
+        {
+            private MethodCallExpression innermostWhereExpression;
+
+            public MethodCallExpression GetInnermostWhere(Expression expression)
+            {
+                Visit(expression);
+                return innermostWhereExpression;
+            }
+
+            protected override Expression VisitMethodCall(MethodCallExpression expression)
+            {
+                if (expression.Method.Name == "Where")
+                    innermostWhereExpression = expression;
+
+                Visit(expression.Arguments[0]);
+
+                return expression;
+            }
         }
     }
 }
