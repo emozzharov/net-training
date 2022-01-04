f370945b39b1b16f4271281bab040c26bbccedaa
Peter Sviryd <Peter.blr@yandex.ru>
2022-01-04 16:46:25 +0300

    finished  DecompressStream && ReadEncodedText

--- 07-IO Streams/IOStreams/TestTasks.cs
+++ 07-IO Streams/IOStreams/TestTasks.cs
@@ -12,88 +12,110 @@ using System.Xml.Linq;
 namespace IOStreams
 {
 
-	public static class TestTasks
-	{
-		/// <summary>
-		/// Parses Resourses\Planets.xlsx file and returns the planet data: 
-		///   Jupiter     69911.00
-		///   Saturn      58232.00
-		///   Uranus      25362.00
-		///    ...
-		/// See Resourses\Planets.xlsx for details
-		/// </summary>
-		/// <param name="xlsxFileName">source file name</param>
-		/// <returns>sequence of PlanetInfo</returns>
-		public static IEnumerable<PlanetInfo> ReadPlanetInfoFromXlsx(string xlsxFileName)
-		{
-			// TODO : Implement ReadPlanetInfoFromXlsx method using System.IO.Packaging + Linq-2-Xml
+    public static class TestTasks
+    {
+        /// <summary>
+        /// Parses Resourses\Planets.xlsx file and returns the planet data: 
+        ///   Jupiter     69911.00
+        ///   Saturn      58232.00
+        ///   Uranus      25362.00
+        ///    ...
+        /// See Resourses\Planets.xlsx for details
+        /// </summary>
+        /// <param name="xlsxFileName">source file name</param>
+        /// <returns>sequence of PlanetInfo</returns>
+        public static IEnumerable<PlanetInfo> ReadPlanetInfoFromXlsx(string xlsxFileName)
+        {
+            // TODO : Implement ReadPlanetInfoFromXlsx method using System.IO.Packaging + Linq-2-Xml
 
-			// HINT : Please be as simple & clear as possible.
-			//        No complex and common use cases, just this specified file.
-			//        Required data are stored in Planets.xlsx archive in 2 files:
-			//         /xl/sharedStrings.xml      - dictionary of all string values
-			//         /xl/worksheets/sheet1.xml  - main worksheet
+            // HINT : Please be as simple & clear as possible.
+            //        No complex and common use cases, just this specified file.
+            //        Required data are stored in Planets.xlsx archive in 2 files:
+            //         /xl/sharedStrings.xml      - dictionary of all string values
+            //         /xl/worksheets/sheet1.xml  - main worksheet
 
-			throw new NotImplementedException();
-		}
-
-
-		/// <summary>
-		/// Calculates hash of stream using specifued algorithm
-		/// </summary>
-		/// <param name="stream">source stream</param>
-		/// <param name="hashAlgorithmName">hash algorithm ("MD5","SHA1","SHA256" and other supported by .NET)</param>
-		/// <returns></returns>
-		public static string CalculateHash(this Stream stream, string hashAlgorithmName)
-		{
-			// TODO : Implement CalculateHash method
-			throw new NotImplementedException();
-		}
+            throw new NotImplementedException();
+        }
 
 
-		/// <summary>
-		/// Returns decompressed strem from file. 
-		/// </summary>
-		/// <param name="fileName">source file</param>
-		/// <param name="method">method used for compression (none, deflate, gzip)</param>
-		/// <returns>output stream</returns>
-		public static Stream DecompressStream(string fileName, DecompressionMethods method)
-		{
-			// TODO : Implement DecompressStream method
-			throw new NotImplementedException();
-		}
+        /// <summary>
+        /// Calculates hash of stream using specifued algorithm
+        /// </summary>
+        /// <param name="stream">source stream</param>
+        /// <param name="hashAlgorithmName">hash algorithm ("MD5","SHA1","SHA256" and other supported by .NET)</param>
+        /// <returns></returns>
+        public static string CalculateHash(this Stream stream, string hashAlgorithmName)
+        {
+            // TODO : Implement CalculateHash method
+            throw new NotImplementedException();
+        }
 
 
-		/// <summary>
-		/// Reads file content econded with non Unicode encoding
-		/// </summary>
-		/// <param name="fileName">source file name</param>
-		/// <param name="encoding">encoding name</param>
-		/// <returns>Unicoded file content</returns>
-		public static string ReadEncodedText(string fileName, string encoding)
-		{
-			// TODO : Implement ReadEncodedText method
-			throw new NotImplementedException();
-		}
-	}
+        /// <summary>
+        /// Returns decompressed strem from file. 
+        /// </summary>
+        /// <param name="fileName">source file</param>
+        /// <param name="method">method used for compression (none, deflate, gzip)</param>
+        /// <returns>output stream</returns>
+        public static Stream DecompressStream(string fileName, DecompressionMethods method)
+        {
+            // TODO : Implement DecompressStream method
+            FileStream sourceStream = new FileStream(fileName, FileMode.Open);
+
+            switch (method)
+            {
+                case DecompressionMethods.GZip:
+
+                    GZipStream decompressionStream = new GZipStream(sourceStream, CompressionMode.Decompress);
+
+                    return decompressionStream;
 
 
-	public class PlanetInfo : IEquatable<PlanetInfo>
-	{
-		public string Name { get; set; }
-		public double MeanRadius { get; set; }
+                case DecompressionMethods.Deflate:
 
-		public override string ToString()
-		{
-			return string.Format("{0} {1}", Name, MeanRadius);
-		}
+                    DeflateStream decompressionStreamD = new DeflateStream(sourceStream, CompressionMode.Decompress);
 
-		public bool Equals(PlanetInfo other)
-		{
-			return Name.Equals(other.Name)
-				&& MeanRadius.Equals(other.MeanRadius);
-		}
-	}
+                    return decompressionStreamD;
+
+
+                default:
+                    return sourceStream;
+            }
+        }
+
+
+        /// <summary>
+        /// Reads file content econded with non Unicode encoding
+        /// </summary>
+        /// <param name="fileName">source file name</param>
+        /// <param name="encoding">encoding name</param>
+        /// <returns>Unicoded file content</returns>
+        public static string ReadEncodedText(string fileName, string encoding)
+        {
+            // TODO : Implement ReadEncodedText method
+            var result = File.ReadAllText(fileName, Encoding.GetEncoding(encoding));
+
+            return result.ToString();
+        }
+    }
+
+
+    public class PlanetInfo : IEquatable<PlanetInfo>
+    {
+        public string Name { get; set; }
+        public double MeanRadius { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", Name, MeanRadius);
+        }
+
+        public bool Equals(PlanetInfo other)
+        {
+            return Name.Equals(other.Name)
+                && MeanRadius.Equals(other.MeanRadius);
+        }
+    }
 
 
 
