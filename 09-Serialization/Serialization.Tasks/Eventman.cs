6989c89438464b7e195c6c2e95edb2b598673aae
Peter Sviryd <Peter.blr@yandex.ru>
2022-01-06 20:40:06 +0300

    finished eventman

--- 09-Serialization/Serialization.Tasks/Eventman.cs
+++ 09-Serialization/Serialization.Tasks/Eventman.cs
@@ -12,6 +12,7 @@ using System.Xml.Serialization;
 
 namespace Serialization.Tasks
 {
+
     // TODO: Generate InstrumentationManifest class from Serialization.Tests\Resources\eventman.xsd to make it ready for XML serialization
     // Use XSD.EXE tool to generate class source code (or die trying to do it manually)
     // Please make separate properties for Instrumentation, Localization and Metadata sections of xml.
@@ -29,14 +30,13 @@ namespace Serialization.Tasks
     [System.Diagnostics.DebuggerStepThroughAttribute()]
     [System.ComponentModel.DesignerCategoryAttribute("code")]
     [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://schemas.microsoft.com/win/2004/08/events")]
-    [System.Xml.Serialization.XmlRootAttribute(Namespace = "http://schemas.microsoft.com/win/2004/08/events", IsNullable = false)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "http://schemas.microsoft.com/win/2004/08/events", IsNullable = false, ElementName = "instrumentationManifest")]
     public partial class InstrumentationManifest : InstrumentationManifestType
     {
 
         private object[] itemsField;
 
         /// <remarks/>
-        [System.Xml.Serialization.XmlAnyElementAttribute()]
 
         [System.Xml.Serialization.XmlElementAttribute("instrumentation", typeof(InstrumentationType))]
         public InstrumentationType Instrumentation { get; set; }
