0367c450b376154b98e860585baa77cc36afd91d
Peter Sviryd <Peter.blr@yandex.ru>
2022-01-09 14:55:32 +0300

    try GetVectorMultiplyFunction

--- 10-Reflection/Reflection.Tasks/CodeGeneration.cs
+++ 10-Reflection/Reflection.Tasks/CodeGeneration.cs
@@ -22,16 +22,60 @@ namespace Reflection.Tasks
         ///   The function that return scalar product of two vectors
         ///   The generated dynamic method should be equal to static MultuplyVectors (see below).   
         /// </returns>
-        public static Func<T[], T[], T> GetVectorMultiplyFunction<T>() where T : struct {
+        public static Func<T[], T[], T> GetVectorMultiplyFunction<T>() where T : struct
+        {
             // TODO : Implement GetVectorMultiplyFunction<T>.
-            throw new NotImplementedException();
-        } 
+            // Creating a parameter expression.  
+            ParameterExpression first = Expression.Parameter(typeof(T[]), "first");
+            ParameterExpression second = Expression.Parameter(typeof(T[]), "second");
+
+            ParameterExpression counter = Expression.Parameter(typeof(int), "counter");
+
+            // Creating an expression to hold a local variable.
+            ParameterExpression result = Expression.Parameter(typeof(T), "result");
+
+            // Creating a label to jump to from a loop.  
+            LabelTarget label = Expression.Label(typeof(int));
+
+            // Creating a method body.  
+            BlockExpression block = Expression.Block(
+                // Adding a local variable.  
+                new[] { result, counter },
+                // Assigning a constant to a local variable: result = 0  
+                Expression.Assign(result, Expression.Constant(0)),
+                    // Adding a loop.  
+                    Expression.Loop(
+                       // Adding a conditional block into the loop.  
+                       Expression.IfThenElse(
+                           // Condition: value < 1  
+                           Expression.LessThan(counter, Expression.ArrayLength(first)),
+                                // If true: result *= value  
+                                Expression.AddAssign(result,
+                                    Expression.Multiply(Expression.ArrayAccess(first, counter),
+                                                        Expression.ArrayAccess(second, Expression.PostIncrementAssign(counter))
+                                                        )
+                                    ),
+                           // If false, exit the loop and go to the label.  
+                           Expression.Break(label, result)
+                       ),
+                   // Label to jump to.  
+                   label
+                )
+            );
+
+            // Compile and execute an expression tree.  
+            var factorial = Expression.Lambda<Func<T[], T[], T>>(block, first, second).Compile();
+
+            return factorial;
+        }
 
 
         // Static solution to check performance benchmarks
-        public static int MultuplyVectors(int[] first, int[] second) {
+        public static int MultuplyVectors(int[] first, int[] second)
+        {
             int result = 0;
-            for (int i = 0; i < first.Length; i++) {
+            for (int i = 0; i < first.Length; i++)
+            {
                 result += first[i] * second[i];
             }
             return result;
